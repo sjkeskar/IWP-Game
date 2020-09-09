eabd61359bc49f33147fb12eefe375218c05f7d6
sjkeskar <sjkeskar@gmail.com>
2020-09-09 20:34:27 +0530

    name changes and css

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const btn = document.querySelector(".donot");
-const clicks = 0;
-
-function over(){
-    var X,Y;
-    var wid = window.innerWidth;
-    var htg = window.innerHeight;
-    do{
-        X = (Math.floor(Math.random()*(wid - btn.clientWidth)+1));
-        Y = (Math.floor(Math.random()*(htg - btn.clientHeight)+1));
-    }while( (X>=wid) && (Y>=htg) );
-    btn.style.setProperty("left",X+"px");
-    btn.style.setProperty("top",Y+"px");
-    console.log(X+","+Y);
-}
-
-function stilldid(){
-    window.open("./result.html");
-}
+setTimeout(function(){
+    window.open("./game.html","_self")
+}, 15000)
